942151e3e0b63b16a5bfe23fbcb55f3b19b1d634
erwann-lubrano <61875357+erwann-lubrano@users.noreply.github.com>
2022-12-15 09:21:37 +0100

    componets

--- POKEISEN/src/app/app.module.ts
+++ POKEISEN/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PokedexPageComponent } from './pokedex-page/pokedex-page.component';
+import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    PokedexPageComponent,
+    PokemonPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
