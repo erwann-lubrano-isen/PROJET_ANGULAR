c93e8b4969ccfddbf98643f91cc1fae1e4d4bd03
erwann-lubrano <61875357+erwann-lubrano@users.noreply.github.com>
2022-12-15 09:49:57 +0100

    routing

--- POKEISEN/src/app/app-routing.module.ts
+++ POKEISEN/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { PokedexPageComponent } from './pokedex-page/pokedex-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'home', component: HomePageComponent },
+	{ path: 'pokedex', component: PokedexPageComponent },
+	{ path: 'about', component: AboutPageComponent },
+    { path: 'pokedex/:id', component: PokemonPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
