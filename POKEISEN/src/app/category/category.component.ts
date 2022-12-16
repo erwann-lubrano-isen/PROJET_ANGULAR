bb41bbc809b2aace4eb6ada3382e989dc43306ed
IdrisseAA <idrisseaa93@gmail.com>
2022-12-16 18:03:15 +0100

    category

--- POKEISEN/src/app/category/category.component.ts
+++ POKEISEN/src/app/category/category.component.ts
@@ -6,30 +6,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  @Input() categories : any;
-   category_types=
-   [
-    {
-      name : "status",
-      url_file : "https://www.smogon.com/dex/media/images/non-damaging.svg"
-    },
-    {
-      name : "special",
-      url_file : "https://www.smogon.com/dex/media/images/non-damaging.svg"
-    },
-    {name : "physical",
-    url_file : "https://www.smogon.com/dex/media/images/physical.svg"
-    }
-  ]
-
-  formated_categories = new Array<any>();
+  @Input() category : string = "status";
 
   constructor() {
-    for(let c of this.categories){
-		  for(let ic of this.category_types){
-			  if(ic.name == c.category.name)this.formated_categories.push(ic);
-		  }
-	  }
    }
 
   ngOnInit(): void {
